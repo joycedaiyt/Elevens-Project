4445e5df6167616ff9866d6d6b8868cd0823f483
Joyce Dai <77522999+joycedaiyt@users.noreply.github.com>
2021-01-16 09:55:36 -0800

    Update CardGameGUI.java

--- Workspace/CardGameGUI.java
+++ Workspace/CardGameGUI.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
  * 03-26-15 slightly altered by Leon Schram 
  * who likes curly braces aligned.
  *
- * DO NOT ALTER THIS CLASS FOR LAB16!!!!!
  */
 public class CardGameGUI extends JFrame implements ActionListener 
 {
