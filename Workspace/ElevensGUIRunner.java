5e6206d477f73ce60a763cf35ab963b51f9a2946
Joyce Dai <77522999+joycedaiyt@users.noreply.github.com>
2021-01-16 09:54:31 -0800

    Update ElevensGUIRunner.java

--- Workspace/ElevensGUIRunner.java
+++ Workspace/ElevensGUIRunner.java
@@ -6,7 +6,6 @@
  * 03-26-15 slightly altered by Leon Schram 
  * who likes curly braces aligned.
  ************************************************************************
- * DO NOT ALTER THIS CLASS FOR LAB16!!!!!
  */
 public class ElevensGUIRunner 
 {
